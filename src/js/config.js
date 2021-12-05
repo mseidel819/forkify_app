01f976d810f7d1f4fd8fc55a4346137ddd69c9b3
mseidel819 <mseidel819@gmail.com>
2021-12-04 18:38:47 -0600

    initial comit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1c1cebbb-9c9b-4b07-a041-a8f334f78c6f';
+export const MODAL_CLOSE_SEC = 2.5;
